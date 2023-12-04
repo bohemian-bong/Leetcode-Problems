c720644d49b794f1b89c9437010d1772b8ace6d5
bohemian-bong <147497706+bohemian-bong@users.noreply.github.com>
2023-12-05 02:02:03 +0530

    Create Solution.cpp

---
+++ Sort Colors/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        for(int i=1; i<nums.size();i++)
+        {
+            int j=i;
+            while(j>0 && nums[j]<nums[j-1])
+            {
+                int temp=nums[j];
+                nums[j]=nums[j-1];
+                nums[j-1]=temp;
+                j--;
+            }
+        }
+    }
+};
